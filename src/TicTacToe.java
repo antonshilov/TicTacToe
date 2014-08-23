f1c3daae4bb616137044fc22962c51f81e4b874b
Anton Shilov <vaccum121@gmail.com>
2014-08-24 00:26:15 +0900

    Revert "Add method center()"
    This reverts commit 76580569447b64ed892577c256b203ecf68f9b18.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,6 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
-//TODO: Add commets, change constructor(Show dialog before show Frame
+
 /**
  * Created by vaccum1 on 21.08.2014.
  * Класс главного окна игры крестики-нолики
@@ -13,15 +13,16 @@ private static final byte FIELD_SIZE = 3;
 private static final String FRAME_NAME = "The Tic-Tac-Toe Game";
 
 
+
+
     public TicTacToe(){
         super(FRAME_NAME);
         Dimension frameSize = new Dimension(600, 600);
         setSize(frameSize);
-        setResizable(false);
+        setResizable(true);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         panel.setLayout(new GridLayout(3, 3));
         this.add(panel);
-        center(this);
 
 
 
@@ -30,19 +31,7 @@ private static final String FRAME_NAME = "The Tic-Tac-Toe Game";
                 buttons[i][j] = new XOButton();
                 panel.add(buttons[i][j]);
             }
-        new ChooseOpponentTypeDialog(this);
         setVisible(true);
     }
-
-    public static void center(JFrame frame) {
-        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-        int w = frame.getSize().width;
-        int h = frame.getSize().height;
-        int x = (dim.width - w) / 2;
-        int y = (dim.height - h) / 2;
-
-        frame.setLocation(x, y);
-    }
-
 
 }
